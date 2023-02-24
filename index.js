9887e7cf6f904b4bf471d52a8c74bea7b763cb3e
Dan Uhl <duhl@wayfair.com>
2023-02-24 14:26:26 -0500

    adds file to do the thing

---
+++ index.js
@@ -0,0 +1,31 @@
+const IMAGE_URL = "https://deepdreamgenerator.com/";
+const cheerio = require("cheerio");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const homedir = require("os").homedir();
+
+function getImage() {
+  return axios.get(IMAGE_URL).then(function({ data }) {
+    // scrape the image
+    let $ = cheerio.load(data);
+    const firstImageUrl = $(".container .content img:first")[0].attribs.src;
+    // save the image
+    axios({
+      method: "get",
+      url: firstImageUrl,
+      responseType: "stream",
+    }).then(function(response) {
+      response.data.pipe(
+        fs.createWriteStream(
+          path.join(homedir, ".warp/themes/night_owl/deep.jpg"),
+          {
+            flags: "w",
+          }
+        )
+      );
+    });
+  });
+}
+
+getImage().catch();
