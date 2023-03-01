2b98ebe1ead8b639b7a9d356a5d31374a36c7e53
Dan Uhl <duhl@wayfair.com>
2023-03-01 10:23:00 -0500

    update to universal shebang

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-#!/usr/bin/env node
+#!/bin/sh
+':'; //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
 
-const IMAGE_SITE_URL = "https://deepdreamgenerator.com/";
-const cheerio = require("cheerio");
-const axios = require("axios");
-const fs = require("fs");
-const path = require("path");
-const homedir = require("os").homedir();
+const IMAGE_SITE_URL = 'https://deepdreamgenerator.com/';
+const cheerio = require('cheerio');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const homedir = require('os').homedir();
 
-const argv = require("yargs/yargs")(process.argv.slice(2)).argv;
+const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
 
-function getImage(theme = "night_owl") {
+function getImage(theme = 'night_owl') {
   console.log(`Fetching new background for your warp theme ${theme}`);
   return axios.get(IMAGE_SITE_URL).then(function({ data }) {
     // scrape the image
     let $ = cheerio.load(data);
 
-    const images = $(".container .content img.light-gallery-item");
+    const images = $('.container .content img.light-gallery-item');
     // grab one of the top half of all the images on the main page
     const imageIndex = Math.floor((Math.random() * images.length) / 2);
     console.log(images[imageIndex].attribs);
@@ -23,15 +24,15 @@ function getImage(theme = "night_owl") {
     console.log({ firstImageUrl });
     // save the image
     axios({
-      method: "get",
+      method: 'get',
       url: firstImageUrl,
-      responseType: "stream",
+      responseType: 'stream',
     }).then(function(response) {
       response.data.pipe(
         fs.createWriteStream(
           path.join(homedir, `.warp/themes/${theme}/deep.jpg`),
           {
-            flags: "w",
+            flags: 'w',
           }
         )
       );
