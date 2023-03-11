bf3da7b90ca01ff1a19902486ba79ea63bfde16e
Dan Uhl <duhl@wayfair.com>
2023-03-11 18:15:10 -0500

    fix update day cache

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 // #!/bin/sh
 // ":"; //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
 
-const IMAGE_SITE_URL = 'https://deepdreamgenerator.com/';
-const cheerio = require('cheerio');
-const axios = require('axios');
-const fs = require('fs-extra');
-const path = require('path');
-const homedir = require('os').homedir();
+const IMAGE_SITE_URL = "https://deepdreamgenerator.com/";
+const cheerio = require("cheerio");
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+const homedir = require("os").homedir();
 
-const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+const argv = require("yargs/yargs")(process.argv.slice(2)).argv;
 
-const THEME_NAME = 'random_bg';
-const THEME_PATH = path.join(homedir, '.warp/themes', THEME_NAME);
+const THEME_NAME = "random_bg";
+const THEME_PATH = path.join(homedir, ".warp/themes", THEME_NAME);
 // if the theme doesn't exist, create it
 function checkAndAddTheme() {
   const templatePath = path.join(__dirname, THEME_NAME);
@@ -21,36 +21,40 @@ function checkAndAddTheme() {
       fs.copySync(templatePath, THEME_PATH, { overwrite: true });
     }
   } catch (e) {
-    console.log('Failed to check if theme exists', e);
+    console.log("Failed to check if theme exists", e);
   }
 }
 
 function didAlreadyFetch() {
-  const { lastFetchedDate } = JSON.parse(
-    fs.readFileSync(path.join(THEME_PATH, '.random_bg_data.json'))
+  const { lastFetchedDay } = JSON.parse(
+    fs.readFileSync(path.join(THEME_PATH, ".random_bg_data.json"))
   );
-  const lastDate = new Date(lastFetchedDate).getDay();
   const today = new Date().getDay();
-  console.log({ lastDate, today });
+  console.log({ lastFetchedDay, today });
+  // update the date in the file
+  fs.writeFileSync(
+    path.join(THEME_PATH, ".random_bg_data.json"),
+    JSON.stringify({ lastFetchedDay: today })
+  );
   // fetch only once per day
-  return lastDate === today;
+  return lastFetchedDay === today;
 }
 
 function saveImage(imageUrl, savePath) {
   axios({
-    method: 'get',
+    method: "get",
     url: imageUrl,
-    responseType: 'stream',
+    responseType: "stream",
   })
     .then(function(response) {
       response.data.pipe(
         fs.createWriteStream(savePath, {
-          flags: 'w',
+          flags: "w",
         })
       );
     })
     .catch((e) => {
-      console.log('err on saving image', e);
+      console.log("err on saving image", e);
     });
 }
 
@@ -60,14 +64,14 @@ function getImage() {
     // scrape the image url
     let $ = cheerio.load(data);
 
-    const images = $('.container .content img.light-gallery-item');
+    const images = $(".container .content img.light-gallery-item");
     console.log(`found ${images.length} images...`);
     // grab one of the top half of all the images on the main page
     const imageIndex = Math.floor((Math.random() * images.length) / 2);
     let firstImageUrl = images[imageIndex].attribs.src;
     if (!firstImageUrl) {
       // sometimes its data-src
-      firstImageUrl = images[imageIndex].attribs['data-src'];
+      firstImageUrl = images[imageIndex].attribs["data-src"];
     }
     console.log({ firstImageUrl });
     return firstImageUrl;
@@ -83,7 +87,7 @@ function main(force) {
     });
   } else {
     console.log(
-      'Looks like we already fetched a new background image today, run with --force to force a new fetch'
+      "Looks like we already fetched a new background image today, run with --force to force a new fetch"
     );
   }
 }
